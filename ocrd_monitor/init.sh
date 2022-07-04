246c93ad92963e7c5549d15a77302ef1e023b5d7
Robert Sachunsky <sachunsky@informatik.uni-leipzig.de>
2022-07-04 08:51:20 +0200

    Monitor: add /jobs and /logs…
    - expose :8088 with Dozzle container for all Docker logs
    - allow connecting to Controller (as from Manager)
    - serve / as index page, pointing to /data, /jobs and /logs
    - serve /data by presenting workspaces (as before)
    - serve /jobs by parsing job info files and running `ps` on Controller
    - serve /logs by delegating to Dozzle
    - serve /conf by showing workflow as `html:pre`

--- ocrd_monitor/init.sh
+++ ocrd_monitor/init.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/env bash
 
-set -x
+mkdir -p ~/.ssh
+cat /id_rsa >> ~/.ssh/id_rsa
+chmod go-rw ~/.ssh/id_rsa
+
+# Add ocrd controller as global and  known_hosts if env exist
+if [ -n "$CONTROLLER" ]; then
+  CONTROLLER_HOST=${CONTROLLER%:*}
+  CONTROLLER_PORT=${CONTROLLER#*:}
+  CONTROLLER_IP=$(nslookup $CONTROLLER_HOST | grep 'Address\:' | awk 'NR==2 {print $2}')
+
+  if test -e /etc/ssh/ssh_known_hosts; then
+    ssh-keygen -R $CONTROLLER_HOST -f /etc/ssh/ssh_known_hosts
+    ssh-keygen -R $CONTROLLER_IP -f /etc/ssh/ssh_known_hosts
+  fi
+  ssh-keyscan -H -p ${CONTROLLER_PORT:-22} $CONTROLLER_HOST,$CONTROLLER_IP >>/etc/ssh/ssh_known_hosts
+fi
+
 broadwayd :5 &
-python serve.py -p 8080 -P 8085 -d "${1:-/data}"
+python -u serve.py -p 8080 -P 8085 -d "${1:-/data}"
