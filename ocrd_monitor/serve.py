435ba247333e217aafbc3706bef1205855c449db
Robert Sachunsky <sachunsky@informatik.uni-leipzig.de>
2022-07-04 12:23:44 +0200

    Monitor: re-use HTML skeleton

--- ocrd_monitor/serve.py
+++ ocrd_monitor/serve.py
@@ -75,22 +75,20 @@ class RequestHandler(SimpleHTTPRequestHandler):
             paths.append(str(mets))
         return paths
 
-    def _serve_index(self):
+    def _serve(self, title, content, headers=''):
         # generate HTML with links
         enc = sys.getfilesystemencoding()
-        title = 'OCR-D Monitor'
         r = []
         r.append('<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" '
                  '"http://www.w3.org/TR/html4/strict.dtd">')
         r.append('<html>\n<head>')
         r.append('<meta http-equiv="Content-Type" '
-                 'content="text/html; charset=%s">' % enc)
+                 'content="text/html; charset=%s"/>' % enc)
+        r.append(headers)
         r.append('<title>%s</title>\n</head>' % title)
         r.append('<body>\n<h1>%s</h1>' % title)
         r.append('<hr>\n\n')
-        r.append('<h2><a href="/data">Show data</a></h2>\n')
-        r.append('<h2><a href="/jobs">Show jobs</a></h2>\n')
-        r.append('<h2><a href="/logs">Show logs</a></h2>\n')
+        r.append(content)
         r.append('<hr>\n</body>\n</html>\n')
         # write to file
         encoded = '\n'.join(r).encode(enc, 'surrogateescape')
@@ -104,32 +102,30 @@ class RequestHandler(SimpleHTTPRequestHandler):
         self.end_headers()
         self.copyfile(f, self.wfile)
 
+    def _serve_index(self):
+        title = 'OCR-D Monitor'
+        content = ('<h2><a href="/data">Show data</a></h2>\n'
+                   '<h2><a href="/jobs">Show jobs</a></h2>\n'
+                   '<h2><a href="/logs">Show logs</a></h2>\n')
+        self._serve(title, content)
+
     def _serve_tasks(self):
-        # generate HTML with links
-        # FIXME: live update via websockets etc
-        # FIXME: show WORKDIR/ocrd.log via log-viewer endpoint
-        enc = sys.getfilesystemencoding()
+        # FIXME: make resource consumption cumulative/persistent
+        # FIXME: also show WORKDIR/ocrd.log via extra log-viewer endpoint
         title = 'OCR-D Tasks'
-        r = []
-        r.append('<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" '
-                 '"http://www.w3.org/TR/html4/strict.dtd">')
-        r.append('<html>\n<head>')
-        r.append('<meta http-equiv="Content-Type" '
-                 'content="text/html; charset=%s">' % enc)
-        r.append('<title>%s</title>\n</head>' % title)
-        r.append('<body>\n<h1>%s</h1>' % title)
-        r.append('<hr>\n<table>\n<tr>')
-        r.append('<th>PID</th>\n'
-                 '<th>STATUS</th>\n'
-                 '<th>% CPU</th>\n'
-                 '<th>MB RSS</th>\n'
-                 '<th>DURATION</th>\n'
-                 '<th>TASK ID</th>\n'
-                 '<th>PROCESS ID</th>\n'
-                 '<th>WORKSPACE</th>\n'
-                 '<th>WORKFLOW</th></tr>\n')
+        content = ('<hr>\n<table>\n<tr>\n'
+                   '<th>PID</th>\n'
+                   '<th>STATUS</th>\n'
+                   '<th>% CPU</th>\n'
+                   '<th>MB RSS</th>\n'
+                   '<th>DURATION</th>\n'
+                   '<th>TASK ID</th>\n'
+                   '<th>PROCESS ID</th>\n'
+                   '<th>WORKSPACE</th>\n'
+                   '<th>WORKFLOW</th></tr>\n')
         for pidfile in self._jobs:
-            r.append('<tr>\n')
+            content += '<tr>\n'
+            # read job info file
             job = dotenv_values(pidfile)
             remotedir = job['REMOTEDIR']
             workdir = job['WORKDIR']
@@ -138,6 +134,7 @@ class RequestHandler(SimpleHTTPRequestHandler):
             workflow = job['WORKFLOW']
             if workflow[0] != '/':
                 workflow = os.path.join(self.basedir, workflow)
+            # log into Controller to list all child processes of job script
             controller = os.environ['CONTROLLER']
             command = 'ssh -T -p %s ocrd@%s' % tuple(controller.split(':')[::-1])
             script = ('DIR=%s; test -e $DIR/ocrd.pid || exit 1\n'
@@ -146,6 +143,7 @@ class RequestHandler(SimpleHTTPRequestHandler):
             result = subprocess.run(command, input=script,
                                     shell=True, stdout=subprocess.PIPE,
                                     universal_newlines=True, encoding='utf-8')
+            # summarize all children's resource consumption
             if result.returncode == 0 and result.stdout:
                 pid = '?'
                 status = '?'
@@ -181,105 +179,53 @@ class RequestHandler(SimpleHTTPRequestHandler):
                 duration = str(duration)
             else:
                 pid, status, cpuutil, memrss, duration = '?', 'terminated', '?', '?', '??:??:??'
-            r.append('<td>%s</td>\n' % pid)
-            r.append('<td>%s</td>\n' % status)
-            r.append('<td>%s</td>\n' % cpuutil)
-            r.append('<td>%s</td>\n' % memrss)
-            r.append('<td>%s</td>\n' % duration)
-            r.append('<td>%s</td>\n' % job['TASK_ID'])
-            r.append('<td>%s</td>\n' % job['PROCESS_ID'])
-            print(workdir)
+            content += ('<td>%s</td>\n' % pid)
+            content += ('<td>%s</td>\n' % status)
+            content += ('<td>%s</td>\n' % cpuutil)
+            content += ('<td>%s</td>\n' % memrss)
+            content += ('<td>%s</td>\n' % duration)
+            content += ('<td>%s</td>\n' % job['TASK_ID'])
+            content += ('<td>%s</td>\n' % job['PROCESS_ID'])
+            # present workspace (as link if available locally)
             if os.path.exists(os.path.join(workdir, 'mets.xml')):
-                r.append('<td><a href="/browse/%s">%s</a></td>'
+                content += ('<td><a href="/browse/%s">%s</a></td>'
                          % (urllib.parse.quote(os.path.relpath(workdir, self.basedir), errors='surrogatepass'),
                             html.escape(os.path.basename(job['PROCESS_DIR']), quote=False)))
             else:
-                r.append('<td/>\n')
-            print(workflow)
+                content += ('<td/>\n')
+            # present workflow (as link if available locally)
             if os.path.exists(workflow):
-                r.append('<td><a href="/conf/%s">%s</a></td>\n'
+                content += ('<td><a href="/conf/%s">%s</a></td>\n'
                          % (urllib.parse.quote(os.path.relpath(workflow, self.basedir), errors='surrogatepass'),
                             html.escape(os.path.basename(workflow), quote=False)))
             else:
-                r.append('<td>%s</td>\n' % os.path.basename(workflow))
-        r.append('</table>\n<hr>\n</body>\n</html>\n')
-        # write to file
-        encoded = '\n'.join(r).encode(enc, 'surrogateescape')
-        f = io.BytesIO()
-        f.write(encoded)
-        f.seek(0)
-        # send file
-        self.send_response(HTTPStatus.OK)
-        self.send_header("Content-type", "text/html; charset=%s" % enc)
-        self.send_header("Content-Length", str(len(encoded)))
-        self.end_headers()
-        self.copyfile(f, self.wfile)
+                content += ('<td>%s</td>\n' % os.path.basename(workflow))
+        content += ('</table>\n')
+        self._serve(title, content, headers='<meta http-equiv="refresh" content="30"/>')
 
     def _configure_workflow(self, path):
-        # generate HTML with links
-        enc = sys.getfilesystemencoding()
         title = 'OCR-D Workflow Configuration'
-        r = []
-        r.append('<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" '
-                 '"http://www.w3.org/TR/html4/strict.dtd">')
-        r.append('<html>\n<head>')
-        r.append('<meta http-equiv="Content-Type" '
-                 'content="text/html; charset=%s">' % enc)
-        r.append('<title>%s</title>\n</head>' % title)
-        r.append('<body>\n<h1>%s</h1>' % title)
-        r.append('<hr>\n\n')
-        r.append('<pre>\n')
+        content = '<pre>\n'
         with open(path, 'r') as workflow:
             text = workflow.read()
             text = html.escape(text, quote=False)
-            r.append(text)
-        r.append('</pre>\n')
-        r.append('<hr>\n</body>\n</html>\n')
-        # write to file
-        encoded = '\n'.join(r).encode(enc, 'surrogateescape')
-        f = io.BytesIO()
-        f.write(encoded)
-        f.seek(0)
-        # send file
-        self.send_response(HTTPStatus.OK)
-        self.send_header("Content-type", "text/html; charset=%s" % enc)
-        self.send_header("Content-Length", str(len(encoded)))
-        self.end_headers()
-        self.copyfile(f, self.wfile)
+            content += text
+        content += '</pre>\n'
+        self._serve(title, content)
 
     def _serve_workspaces(self):
-        # generate HTML with links
-        enc = sys.getfilesystemencoding()
         basedir = os.path.realpath(self.basedir)
         title = html.escape(basedir, quote=False)
         title = 'OCR-D Browser for workspaces at %s' % title
-        r = []
-        r.append('<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" '
-                 '"http://www.w3.org/TR/html4/strict.dtd">')
-        r.append('<html>\n<head>')
-        r.append('<meta http-equiv="Content-Type" '
-                 'content="text/html; charset=%s">' % enc)
-        r.append('<title>%s</title>\n</head>' % title)
-        r.append('<body>\n<h1>%s</h1>' % title)
-        r.append('<hr>\n<ul>')
+        content = '<ul>\n'
         for name in self._workspaces:
             relname = os.path.relpath(name, basedir)
             linkname = os.path.join('/browse', relname)
-            r.append('<li><a href="%s">%s</a></li>'
-                     % (urllib.parse.quote(linkname, errors='surrogatepass'),
-                        html.escape(relname, quote=False)))
-        r.append('</ul>\n<hr>\n</body>\n</html>\n')
-        # write to file
-        encoded = '\n'.join(r).encode(enc, 'surrogateescape')
-        f = io.BytesIO()
-        f.write(encoded)
-        f.seek(0)
-        # send file
-        self.send_response(HTTPStatus.OK)
-        self.send_header("Content-type", "text/html; charset=%s" % enc)
-        self.send_header("Content-Length", str(len(encoded)))
-        self.end_headers()
-        self.copyfile(f, self.wfile)
+            content += ('<li><a href="%s">%s</a></li>'
+                        % (urllib.parse.quote(linkname, errors='surrogatepass'),
+                           html.escape(relname, quote=False)))
+        content += '</ul>\n'
+        self._serve(title, content)
 
     def _browse_workspace(self, path):
         if not os.path.exists(path):
